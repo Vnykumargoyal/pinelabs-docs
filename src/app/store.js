bd3987b86f64d08ebf9667508da49d7fd80ba2ce
vinay-goyal_pinegit <vinay.goyal@pinelabs.com>
2026-04-24 12:41:06 +0530

    Remove stale files, clean workspace

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useDocStore = create((set) => ({
-  activeSection: "",
-  language: "curl",
-
-  setSection: (id) => set({ activeSection: id }),
-  setLanguage: (lang) => set({ language: lang }),
-}));
